bbd47c7d6af7a2d6be7094fe9f01d4b3c8c4e4ca
Maxim Tsarev <noo.free.names@gmail.com>
2022-02-07 00:52:08 +0500

    Created the grid of the program window Added a MainLayout.psd file representing an approximate window layout. The layout has been moved to the WPF application.

---
+++ SAE/mainWin/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mainWin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            const float sizeFactor = 0.7F;
+            double ScreenW = SystemParameters.PrimaryScreenWidth;
+            double ScreenH = SystemParameters.PrimaryScreenHeight;
+
+            /*Setting the window size*/
+            MainWin.Width = ScreenW * sizeFactor;
+            MainWin.Height = ScreenH * sizeFactor;
+
+            /*Setting the window in the center of the screen*/
+            MainWin.Left = (ScreenW - MainWin.Width) / 2;
+            MainWin.Top = (ScreenH - MainWin.Height) / 2;
+        }
+    }
+
+}
